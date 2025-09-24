2eccac03d16f62723a465b453d04e20594462a58
Rohit Dharmadhikari <156362091+Rohitcd-47@users.noreply.github.com>
2025-09-24 17:13:30 +0530

    Add files via upload

---
+++ Class-work/demo.c
@@ -0,0 +1,33 @@
+// Writing a program to print addition of 2 numbers
+
+#include <stdio.h>
+void main()
+{
+	int a,b,c;
+	a = 10, b = 2, c = a+b;
+	printf("Addition of %d and %d is %d ",a,b,c);  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
